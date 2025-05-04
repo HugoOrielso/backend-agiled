93a1854b5db5e8ada0c4761b1598c79e8b857745
Hugo Orielso <orielso.lozano15@gmail.com>
2025-05-03 23:29:36 -0500

    pequeñas actualizaciones

--- index.js
+++ index.js
@@ -11,22 +11,24 @@ app.use(express.json())
 app.post('/api/create-contact', async (req, res) => {
     const apiToken = process.env.AGILED_API_TOKEN
     const brand = process.env.AGILED_BRAND
-    const contact = req.body
-    contact.role = "Lead"
+    const { first_name, last_name, email, phone, project_scope, address, tags, note } = req.body;
+
+    const contact = { first_name, last_name, email, phone, project_scope, address, tags, note, role: "Lead" };
 
     try {
         const response = await fetch(`https://my.agiled.app/api/v1/contacts?api_token=${apiToken}`, {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Brand': `${brand}`,
-                'X-CSRF-TOKEN': ''
+                'Accept': "application/json"
             },
             body: JSON.stringify(contact)
         })
 
         const data = await response.json()
+        console.log(data);
+
         res.status(200).json(data)
     } catch (error) {
         console.error(error)
@@ -34,4 +36,5 @@ app.post('/api/create-contact', async (req, res) => {
     }
 })
 
-export default app  
+export default app 
+
