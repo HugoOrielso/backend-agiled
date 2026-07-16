0fa8496ddb5aa47b364c8b7cb39d1a8d6e3d90c5
Hugo Orielso <orielso.lozano15@gmail.com>
2026-07-16 16:35:17 -0500

    email changed

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post("/api/form-submitted", async (req, res) => {
         process.env.RESEND_FROM_EMAIL ||
         "Moreno Stucco <onboarding@hugoorielso.com>",
       to:
-        "orielso.lozano15@gmail.com",
+        "info@morenostucco.com",
       subject: "New Book a Visit request",
       html: `
         <div style="margin:0;padding:0;background:#f3f6fb;font-family:Arial,Helvetica,sans-serif;color:#111827;">
